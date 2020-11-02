4395eb0755711b1f2b2d4faefcbe102d3e68ff4a
p-adrian05 <adrian16616@gmail.com>
2020-11-02 09:52:36 +0100

    Add WebConfig.class to config package

---
+++ webapp/src/main/java/szfm/errorbynight/config/WebConfig.java
@@ -0,0 +1,17 @@
+package szfm.errorbynight.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localResolver(){
+        return new SessionLocaleResolver();
+    }
+
+}

c287c882d91e2a848991f7ede989c41305232dfe
p-adrian05 <adrian16616@gmail.com>
2020-11-07 10:07:47 +0100

    Add SecurityConfig class to config package for custom security configuration

--- webapp/src/main/java/szfm/errorbynight/config/SecurityConfig.java
+++ webapp/src/main/java/szfm/errorbynight/config/SecurityConfig.java
@@ -26,31 +26,15 @@ import java.util.Properties;
 @PropertySource("classpath:email.properties")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private Environment env;
-
     @Qualifier("userServiceImpl")
     @Autowired
     private UserDetailsService userDetailsService;
 
-    @Autowired
-    private AuthenticationSuccessHandler authenticationSuccessHandler;
 
-    /**
-     *Felhasznalok beconfiguralasa
-     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
-
-    /**
-     * HTTPS keresek szurese
-     * Eloszor minden lezar a szerveren majd engedelyezzuk
-     * CSRF - Cross site request forgery
-     * random generalt tokent kuld a server a kuldott formhoz, rejtve
-     * hiteles forrast ellenorzi ez
-     */
 
     @Override
     protected void configure(HttpSecurity httpSec) throws Exception {
@@ -61,7 +45,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/home").permitAll()
                 .antMatchers("/themes/**").permitAll()
                 .antMatchers("/theme/**").permitAll()
-                .antMatchers("/resources/**").permitAll()
                 .antMatchers("/account/profile/**").permitAll()
                 .antMatchers("/css/**").permitAll()
                 .antMatchers("/img/**").permitAll()
@@ -72,7 +55,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .formLogin()
             .loginPage("/login")
             .permitAll()
-                .successHandler(authenticationSuccessHandler)
         .and()
         .logout()
             .logoutSuccessUrl("/?logout")
@@ -81,22 +63,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(env.getProperty("mail.host"));
-        mailSender.setPort(Integer.parseInt(env.getProperty("mail.port")));
-
-        mailSender.setUsername(env.getProperty("mail.username"));
-        mailSender.setPassword(env.getProperty("mail.password"));
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", env.getProperty("mail.transport.protocol"));
-        props.put("mail.smtp.auth", env.getProperty("mail.smtp.auth"));
-        props.put("mail.smtp.starttls.enable", env.getProperty("mail.smtp.starttls.enable"));
-        props.put("mail.debug",  env.getProperty("mail.debug"));
-        return mailSender;
     }
 }

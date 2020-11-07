f7af7b22e52f1b4bcb28fbc1bf1aa9faee19dbd1
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2020-11-07 16:50:06 +0100

    Added required annotations to FieldsVerification.java

--- webapp/src/main/java/szfm/errorbynight/validation/FieldsVerification.java
+++ webapp/src/main/java/szfm/errorbynight/validation/FieldsVerification.java
@@ -1,5 +1,12 @@
 package szfm.errorbynight.validation;
 
+import javax.validation.Constraint;
+import java.lang.annotation.*;
+
+@Constraint(validatedBy = FieldsVerificationValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
 public @interface FieldsVerification {
 
 }

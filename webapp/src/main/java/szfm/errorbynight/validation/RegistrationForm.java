83498bb35e2c8688e47d78d2f0475536505e62c5
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2020-11-07 17:30:52 +0100

    Added username variable with annotations to RegistrationForm.java

--- webapp/src/main/java/szfm/errorbynight/validation/RegistrationForm.java
+++ webapp/src/main/java/szfm/errorbynight/validation/RegistrationForm.java
@@ -16,4 +16,7 @@ import szfm.errorbynight.validation.FieldsVerification;
         )
 })
 public class RegistrationForm {
+  @Size(min = 2,max = 15,message = "Username: min 2 characters required, max 10 characters allowed")
+  @NotEmpty(message = "Username cannot be empty")
+  private String username;
 }

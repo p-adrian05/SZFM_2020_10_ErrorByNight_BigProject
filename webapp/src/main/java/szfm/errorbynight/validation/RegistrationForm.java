2bef307b3957c6d35cdbb8c3b1e1e04ea7403339
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2020-11-07 17:29:46 +0100

    Added required annotations to RegistrationForm.java

--- webapp/src/main/java/szfm/errorbynight/validation/RegistrationForm.java
+++ webapp/src/main/java/szfm/errorbynight/validation/RegistrationForm.java
@@ -1,4 +1,19 @@
 package szfm.errorbynight.validation;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import szfm.errorbynight.validation.FieldsVerification;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@FieldsVerification.List( value = {
+        @FieldsVerification(
+                field = "password",
+                fieldMatch = "verifyPassword",
+                message = "Passwords do not match"
+        )
+})
 public class RegistrationForm {
 }

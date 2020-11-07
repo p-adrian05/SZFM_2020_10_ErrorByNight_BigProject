00d9cf63c57e5a34e4467844275f6d7963ecf623
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2020-11-07 16:51:47 +0100

    Added code to FieldsVerification.java

--- webapp/src/main/java/szfm/errorbynight/validation/FieldsVerification.java
+++ webapp/src/main/java/szfm/errorbynight/validation/FieldsVerification.java
@@ -2,6 +2,7 @@ package szfm.errorbynight.validation;
 
 import javax.validation.Constraint;
 import java.lang.annotation.*;
+import javax.validation.Payload;
 
 @Constraint(validatedBy = FieldsVerificationValidator.class)
 @Retention(RetentionPolicy.RUNTIME)
@@ -9,4 +10,16 @@ import java.lang.annotation.*;
 @Documented
 public @interface FieldsVerification {
 
+  String message() default "Fields do not match";
+  String field();
+  String fieldMatch();
+
+  Class<?>[] groups() default {};
+  Class<? extends Payload>[] payload() default {};
+
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target(ElementType.TYPE)
+  @interface List{
+    FieldsVerification[] value();
+  }
 }

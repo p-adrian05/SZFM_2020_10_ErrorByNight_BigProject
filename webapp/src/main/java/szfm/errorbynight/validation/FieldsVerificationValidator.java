f2fa14dfbe8d179c691cf437aab79fd38a4f4209
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2020-11-07 17:15:33 +0100

    Added isValid() method to FieldsVerificationValidator.java

--- webapp/src/main/java/szfm/errorbynight/validation/FieldsVerificationValidator.java
+++ webapp/src/main/java/szfm/errorbynight/validation/FieldsVerificationValidator.java
@@ -1,14 +1,27 @@
 package szfm.errorbynight.validation;
 
 import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
 
 public class FieldsVerificationValidator implements ConstraintValidator<FieldsVerification,Object> {
   private String field;
   private String fieldMatch;
 
-  @Override
+    @Override
     public void initialize(FieldsVerification constraintAnnotation) {
         field = constraintAnnotation.field();
         fieldMatch = constraintAnnotation.fieldMatch();
     }
+
+    @Override
+    public boolean isValid(Object o, ConstraintValidatorContext constraintValidatorContext) {
+        Object fieldValue = new BeanWrapperImpl(o).getPropertyValue(field);
+        Object fieldMatchValue = new BeanWrapperImpl(o).getPropertyValue(fieldMatch);
+
+        if(fieldValue !=null){
+            return fieldValue.equals(fieldMatchValue);
+        }
+        return fieldMatchValue == null;
+    }
+}
 }

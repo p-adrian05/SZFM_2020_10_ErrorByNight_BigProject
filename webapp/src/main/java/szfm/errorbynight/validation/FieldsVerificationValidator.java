9b0a65f3eab28ee760003a98eeace25de28774f2
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2020-11-07 17:14:05 +0100

    Added initialize() method to FieldsVerificationValidator.java

--- webapp/src/main/java/szfm/errorbynight/validation/FieldsVerificationValidator.java
+++ webapp/src/main/java/szfm/errorbynight/validation/FieldsVerificationValidator.java
@@ -3,5 +3,12 @@ package szfm.errorbynight.validation;
 import javax.validation.ConstraintValidator;
 
 public class FieldsVerificationValidator implements ConstraintValidator<FieldsVerification,Object> {
+  private String field;
+  private String fieldMatch;
 
+  @Override
+    public void initialize(FieldsVerification constraintAnnotation) {
+        field = constraintAnnotation.field();
+        fieldMatch = constraintAnnotation.fieldMatch();
+    }
 }

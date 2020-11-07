fd04b8d70a5d2bd35de1b97dee3551cac3740d15
p-adrian05 <adrian16616@gmail.com>
2020-11-07 10:18:16 +0100

    Made user sessionAttribute in success handler

--- webapp/src/main/java/szfm/errorbynight/config/CustomAuthenticationSuccessHandler.java
+++ webapp/src/main/java/szfm/errorbynight/config/CustomAuthenticationSuccessHandler.java
@@ -1,17 +1,31 @@
 package szfm.errorbynight.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import szfm.errorbynight.model.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Optional;
 
+@Component
+@SessionAttributes(names = {"currentUser"})
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private HttpSession session;
+
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,

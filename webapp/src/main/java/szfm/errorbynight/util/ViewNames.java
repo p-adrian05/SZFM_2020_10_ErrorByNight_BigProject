f27ee4a25ab4dd5b208bb04dfb2115dc058a9d18
p-adrian05 <adrian16616@gmail.com>
2020-11-10 10:13:30 +0100

    Add intitial ViewNames class for storing view names constants

---
+++ webapp/src/main/java/szfm/errorbynight/util/ViewNames.java
@@ -0,0 +1,14 @@
+package szfm.errorbynight.util;
+
+
+public class ViewNames {
+
+    public static final String REGISTRATION = "registration";
+    public static final String LOGIN = "auth/login";
+    public static final String PROFILE = "profile";
+    public static final String USERDATA = "userdata";
+
+    private ViewNames(){}
+
+    public static final String HOME = "home";
+}

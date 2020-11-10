70a362a8d5efb37d691bfbf11697802282a5c982
p-adrian05 <adrian16616@gmail.com>
2020-11-10 10:22:14 +0100

    Add initial RegistrationController class to controller package

---
+++ webapp/src/main/java/szfm/errorbynight/controller/RegistrationController.java
@@ -0,0 +1,13 @@
+package szfm.errorbynight.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping(Mappings.REGISTRATION)
+public class RegistrationController {
+
+    
+}

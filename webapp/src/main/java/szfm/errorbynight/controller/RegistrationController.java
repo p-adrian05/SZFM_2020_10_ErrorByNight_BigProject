46468d1fd36aade8ee800f2bb753badf864d9cca
p-adrian05 <adrian16616@gmail.com>
2020-11-10 10:26:20 +0100

    Implement user registration in RegistrationController

--- webapp/src/main/java/szfm/errorbynight/controller/RegistrationController.java
+++ webapp/src/main/java/szfm/errorbynight/controller/RegistrationController.java
@@ -3,8 +3,19 @@ package szfm.errorbynight.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.thymeleaf.engine.AttributeNames;
+import szfm.errorbynight.model.User;
 import szfm.errorbynight.services.UserService;
+import szfm.errorbynight.validation.RegistrationForm;
+
+import javax.validation.Valid;
+import java.sql.SQLException;
 
 @Controller
 @Slf4j
@@ -16,5 +27,26 @@ public class RegistrationController {
     @Autowired
     public RegistrationController(UserService userService) {
         this.userService = userService;
+    }
+    @PostMapping("")
+    public String registerUser(@Valid @ModelAttribute RegistrationForm form,
+                               BindingResult bindingResult,
+                               Model model,
+                               RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute(AttributeNames.ERRORS, bindingResult.getAllErrors());
+            return ViewNames.REGISTRATION;
+        } else {
+            try {
+
+                userService.registerUser(new User(form.getUsername(), form.getEmail(), form.getPassword()));
+            } catch (SQLException e) {
+                model.addAttribute(AttributeNames.SQL_ERROR, e.getMessage());
+                log.info(e.getMessage());
+                return ViewNames.REGISTRATION;
+            }
+        }
+        redirectAttributes.addFlashAttribute(AttributeNames.REG_SUCCESS, true);
+        return Mappings.REDIRECT_REGISTRATION;
     }
 }

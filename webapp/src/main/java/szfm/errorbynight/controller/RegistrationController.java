7ce64a5e68a5f796a8c78320f00aae5cfc5281da
p-adrian05 <adrian16616@gmail.com>
2020-11-10 10:22:56 +0100

    Made UserService to be injected in RegistrationController class

--- webapp/src/main/java/szfm/errorbynight/controller/RegistrationController.java
+++ webapp/src/main/java/szfm/errorbynight/controller/RegistrationController.java
@@ -1,13 +1,20 @@
 package szfm.errorbynight.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import szfm.errorbynight.services.UserService;
 
 @Controller
 @Slf4j
 @RequestMapping(Mappings.REGISTRATION)
 public class RegistrationController {
 
-    
+    private final UserService userService;
+
+    @Autowired
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
 }

f6b53732eb7e199734308a1dc101ddac2a0cb8da
p-adrian05 <adrian16616@gmail.com>
2020-11-10 10:15:08 +0100

    Add intitial AttributeName class for store attribute names constants

---
+++ webapp/src/main/java/szfm/errorbynight/util/AttributeNames.java
@@ -0,0 +1,12 @@
+package szfm.errorbynight.util;
+
+public final class AttributeNames {
+
+    private AttributeNames(){}
+
+    public static final String REG_SUCCESS = "registrationSuccess";
+    public static final String SQL_ERROR = "sqlError";
+    public static final String ACTIVATION_RESULT = "activationResult";
+    public static final String ERRORS = "errors";
+    public static final String REG_FORM = "registrationForm";
+}

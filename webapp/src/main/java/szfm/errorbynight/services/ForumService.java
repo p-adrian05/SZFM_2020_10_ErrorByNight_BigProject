5ec66e75361cc3e2b39f7eabe11c27bbca7df626
p-adrian05 <adrian16616@gmail.com>
2020-11-14 09:48:36 +0100

    Made interface for forum service

---
+++ webapp/src/main/java/szfm/errorbynight/services/ForumService.java
@@ -0,0 +1,32 @@
+package szfm.errorbynight.services;
+
+
+import szfm.errorbynight.model.*;
+import szfm.errorbynight.util.ThemeStat;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ForumService {
+
+    boolean addNewTopic(String categoryName, Topic newTopic, User currentUser);
+
+    int getTopicsCount(String themeName);
+    boolean addNewPostToTopic(User currentUser, String topicName, String postMessage);
+
+    int getTopicPostsCount(String topicName);
+
+    List<Post> getPostsByCategoryName(String topicName, int offset, int range);
+
+    boolean savePost(String topicName, Post post, User senderUser);
+
+    List<ThemeStat> getTopicsAndPostsCount(String categoryName, int offset, int range);
+
+    Map<ForumCategory, Integer> getAllForumCategoriesAndTopicsCount();
+
+    List<String> getFavouriteTopicNames(Long userId);
+
+    boolean addNewFavouriteTopic(String topicName, User currentUser);
+
+    boolean deleteFavouriteTopic(String topicName, User currentUser);
+}

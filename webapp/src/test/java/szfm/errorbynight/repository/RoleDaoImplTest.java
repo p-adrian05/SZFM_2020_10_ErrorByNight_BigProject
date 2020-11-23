43b168550dda71b6c4b0aa91d61b952b062f6843
p-adrian05 <adrian16616@gmail.com>
2020-11-23 12:09:46 +0100

    Add dependencies to pom.xml for testing and create test folder

---
+++ webapp/src/test/java/szfm/errorbynight/repository/RoleDaoImplTest.java
@@ -0,0 +1,32 @@
+package szfm.errorbynight.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleDaoImplTest {
+
+    @Test
+    void findByRole() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void save() {
+    }
+}

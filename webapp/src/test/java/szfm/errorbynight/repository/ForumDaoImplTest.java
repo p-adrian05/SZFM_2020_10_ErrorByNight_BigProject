004bf914f75a974513d56ee834c7ae7d896a0170
Remias <remias.david5761@gmail.com>
2020-11-28 12:24:05 +0100

    Added getCategoryByName() test.

---
+++ webapp/src/test/java/szfm/errorbynight/repository/ForumDaoImplTest.java
@@ -0,0 +1,39 @@
+package szfm.errorbynight.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import szfm.errorbynight.WebMain;
+import szfm.errorbynight.config.TestDataSource;
+import szfm.errorbynight.model.ForumCategory;
+import szfm.errorbynight.model.Post;
+import szfm.errorbynight.model.Topic;
+import szfm.errorbynight.util.ThemeStat;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {WebMain.class, TestDataSource.class})
+@ActiveProfiles("test")
+class ForumDaoImplTest {
+
+    @Autowired
+    private ForumDao forumDao;
+
+    @Test
+    void getCategoryByName() {
+         Optional<ForumCategory> forumCategory = forumDao.getCategoryByName("testCategory");
+         if(forumCategory.isPresent()){
+             assertEquals("testCategory",forumCategory.get().getTitle());
+             assertNotEquals(forumCategory.get().getTitle(),"testCategory2");
+         }else{
+             fail("no data");
+         }
+    }
+}

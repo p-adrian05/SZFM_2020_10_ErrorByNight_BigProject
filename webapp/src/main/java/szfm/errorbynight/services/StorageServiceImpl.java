e51070826eae70709b9b044bbad2c67406611b43
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2020-11-30 11:42:48 +0100

    Added loadAsResource() method to StorageServiceImpl.java

--- webapp/src/main/java/szfm/errorbynight/services/StorageServiceImpl.java
+++ webapp/src/main/java/szfm/errorbynight/services/StorageServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.env.Environment;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.core.io.UrlResource;
 import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import szfm.errorbynight.util.UtilService;
 
@@ -15,6 +17,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.net.MalformedURLException;
 
 @Service
 @Slf4j
@@ -50,7 +53,18 @@ public class StorageServiceImpl implements StorageService {
 
   @Override
   public Resource loadAsResource(String filename) {
-
+    Resource resource = null;
+    try{
+      Path file = load(filename);
+      resource = new UrlResource(file.toUri());
+      if(resource.exists() || resource.isReadable()){
+      return resource;
+      }else{
+        throw new ResourceAccessException("Could not read file "+ filename);
+      }
+    }catch (MalformedURLException e){
+      throw new ResourceAccessException("Could not read file "+ filename,e);
+    }
   }
 
   @Override

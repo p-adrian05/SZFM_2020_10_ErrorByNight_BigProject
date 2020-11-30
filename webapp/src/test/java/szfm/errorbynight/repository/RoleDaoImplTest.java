227250b3f1eec82ea24de5349e12de23b8ef9984
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2020-11-30 12:23:59 +0100

    Added findByRole() method to RoleDaoImplTest.java

--- webapp/src/test/java/szfm/errorbynight/repository/RoleDaoImplTest.java
+++ webapp/src/test/java/szfm/errorbynight/repository/RoleDaoImplTest.java
@@ -4,10 +4,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {WebMain.class, TestDataSource.class})
+@ActiveProfiles("test")
 class RoleDaoImplTest {
+
+    @Autowired
+    private RoleDao roleDao;
 
     @Test
     void findByRole() {
+      Role role = roleDao.findByRole("USER").get();
+      assertEquals(role.getRole(),"USER");
     }
 
     @Test

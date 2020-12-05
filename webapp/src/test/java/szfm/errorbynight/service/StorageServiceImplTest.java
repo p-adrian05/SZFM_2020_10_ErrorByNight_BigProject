a6e59e0df80bc4166c340d3e75b45cbd5d9c4aaa
Remias <remias.david5761@gmail.com>
2020-12-05 12:23:18 +0100

    Added StorageServiceImplTest

---
+++ webapp/src/test/java/szfm/errorbynight/service/StorageServiceImplTest.java
@@ -0,0 +1,16 @@
+package szfm.errorbynight.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.ResourceAccessException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class StorageServiceImplTest {
+	
+}
